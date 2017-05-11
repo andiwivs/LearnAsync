4c45b168391025d6556179b12f4a5f5b38ed0dac
Andrew Withers <andiwivs@gmail.com>
2017-05-11 22:27:45 +0100

    Add asp.net with example async action

---
+++ DemoWebApp/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DemoWebApp
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                // Tip: use ConfigureAwait(false) to allow any IIS thread to complete the action
+                var httpMessage = await client.GetAsync("http://www.google.co.uk").ConfigureAwait(false);
+
+                var result = await httpMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                return Content(result);
+            }
+        }
+    }
+}
